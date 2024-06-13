8fd2b0df2bf83cd2b4efa4f0bd7dedf81c09c4f0
rocio <rg3113@yahoo.com>
2024-06-13 15:39:37 -0500

    updated edit err

--- app/editPost/error.js
+++ app/editPost/error.js
@@ -1,22 +1,17 @@
-"use client"; 
+"use client";
 
 import { useEffect } from "react";
 
-export default function Error({ error, reset }) {
+export default function Error({ error }) {
   useEffect(() => {
     console.error(error);
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className=" grid justify-items-center  content-center mx-auto my-10   border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:flex lg:justify-center lg:gap-3  text-gray-900">
+      <div className="errorMessage">
+        <h2>Something went wrong while trying to edit this post. Try again later.</h2>
+      </div>
     </div>
   );
 }
