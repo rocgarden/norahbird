ec7e4d6dee919db4ef186931d43ac3cacb7527f9
rocio <rg3113@yahoo.com>
2024-06-16 11:00:00 -0500

    edit force-dynamic

--- app/api/post/[id]/route.js
+++ app/api/post/[id]/route.js
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 import Post from "@/app/models/PostSchema";
 import mongoose from "mongoose";
 
+export const dynamic = "force-dynamic";
+
 export async function PUT(request, { params }) {
 //   const id = request.nextUrl.searchParams.get("id");
   
