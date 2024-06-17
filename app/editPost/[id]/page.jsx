940788659358a480a1e2248541ed521698af445a
rocio <rg3113@yahoo.com>
2024-06-17 16:00:16 -0500

    updated path

--- app/editPost/[id]/page.jsx
+++ app/editPost/[id]/page.jsx
@@ -2,7 +2,7 @@ import EditPost from "@/app/components/editPostForm";
 
 const getPostById = async(id) => {
    try {
-    const res = await fetch(`/api/post/${id}`, {
+    const res = await fetch(`https://www.norahbird.com//api/post/${id}`, {
       cache: "no-store",
     });
     if (!res.ok) {
